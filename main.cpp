b37115d634dd1eb07eb6d5c0f7ab98f5eb1a78dc
JonathanJiWu <jonathanwubrave@gmail.com>
2022-03-31 23:29:01 -0400

    first push!

---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include<iostream>//input/output stream, for pre-processer, before compile
+
+int main()//return type, function name
+{
+	std::cout << "hole wornd";//statement, std=> namespace =>a group of codes =>pervent naming conflects, cout => object
+	return 0;//return value
+}
