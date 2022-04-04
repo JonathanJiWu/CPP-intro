6f43b2cd03565c856fb16e8e95cd8e55961fc9c6
JonathanJiWu <jonathanwubrave@gmail.com>
2022-04-04 00:47:28 -0400

    dfunction

--- main.cpp
+++ main.cpp
@@ -1,6 +1,19 @@
 #include<iostream>//input/output stream, for pre-processer, before compile
 
+
+void functionOne()//function identifier (name)
+{
+
+}
+
+double multiply(double x, double y)//function declaration(can out in .h)
+{
+	return x * y;//function definition(can put in .cpp)
+}
+
+
 int main()//return type, function name
+//main() is call when the program is run
 {
 	int value = 5;// = is assign operator
 	double newValue;//declaration
@@ -16,6 +29,21 @@ int main()//return type, function name
 	std::cout << "hole wornd"<< std::endl;//statement, std=> namespace =>a group of codes =>pervent naming conflects, cout => object
 	//endl is a Manipulator
 	printf("%i/n", value);// from C, (type, target), old stuff
+
+	int slice;
+
+	std::cout << "say something";
+	std::cin >> slice;// console in
+	std::cout << "you just said" << slice;
+
+	//intro to functions
+	//declaration, definition, calling
+	double pow1 = pow(10, 2);//two arguments
+
+	double pow2 = pow(22, 2) * 2;
+
+	functionOne();//calling
+	multiply(2, 5);//calling
 
 	return 0;//return value
 }
